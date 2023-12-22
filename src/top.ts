872b2b5b2ab48cd0c4dcf525893782698e846daa
Andrey Stepachev <octo47@gmail.com>
2023-12-22 23:58:06 +0000

    New utility top.js

---
+++ src/top.ts
@@ -0,0 +1,40 @@
+import { NS } from '@ns'
+import { Scanner } from 'lib/scanner.js'
+import { tabulate } from '/lib/tabulate';
+
+export async function main(ns : NS) : Promise<void> {
+
+    type InProgress = {
+        "Worker": string,
+        "Threads": number,
+        "Targets": string[],
+    }
+
+    // eslint-disable-next-line no-constant-condition
+    while(true) {
+        const servers = new Scanner()
+        .scan(ns)
+        .hosts
+        .map((item) => ns.getServer(item.hostname))
+
+        const inProgress: {[worker:string]: InProgress} = {}
+
+        servers.forEach((srv) => {
+            const workerState: InProgress = {
+                Worker: srv.hostname,
+                Targets: [],
+                Threads: 0
+            }
+            ns.ps(srv.hostname).forEach((pi) =>  {
+                workerState.Targets.push(pi.args[0].toString())
+                workerState.Threads += pi.threads
+            })
+            inProgress[srv.hostname] = workerState
+        })
+
+        tabulate(ns, Object.keys(inProgress).map(key => inProgress[key]), 
+           ["Worker", "Threads", "Targets"]
+        )
+        await ns.sleep(2000)
+    }
+}
